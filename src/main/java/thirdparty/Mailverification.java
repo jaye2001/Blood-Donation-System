998a73e7474629b3389e0fb34320a7016d3d991a
NidulaJay <nidulajpjayasinghe@gmail.com>
2024-03-24 14:06:22 +0530

    added new dependecy

--- src/main/java/thirdparty/Mailverification.java
+++ src/main/java/thirdparty/Mailverification.java
@@ -2,7 +2,7 @@ package thirdparty;
 import java.util.Properties;  
   
 import javax.mail.*;  
-import javax.mail.internet.InternetAddress;  
+import javax.mail.internet.*;  
 import javax.mail.internet.MimeMessage;  
   
 public class Mailverification {  
