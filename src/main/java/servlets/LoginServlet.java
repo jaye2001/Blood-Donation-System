7bb87eb093370253bf5109d25f873d3055086bb5
NidulaJay <nidulajpjayasinghe@gmail.com>
2024-03-24 14:23:27 +0530

    adwad

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -26,7 +26,7 @@ import thirdparty.*;
 /**
  * Servlet implementation class LoginServlet
  */
-//@WebServlet("/LoginServlet")
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
